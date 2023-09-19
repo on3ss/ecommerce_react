a4dbd312e27ec6e8e18e01225f979f2cdaec71b5
on3ss <on3ss@DESKTOP-UM81EHJ>
2023-09-19 16:40:56 +0530

    added infinite pagination

--- src/pages/home/ProductsList.tsx
+++ src/pages/home/ProductsList.tsx
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import Loader from "../components/Loader";
+import React from "react";
 
 type ProductType = {
     id: number;
@@ -26,8 +27,7 @@ type ProductResponse = {
     limit: number;
 }
 
-function ProductCard(props: ProductCardProp) {
-    const product = props.product
+function ProductCard({ product }: { product: ProductType }) {
     return (
         <a href="#">
             <div className="rounded shadow-sm bg-gray-50">
@@ -55,31 +55,50 @@ function ProductCard(props: ProductCardProp) {
 
 function ProductsList() {
 
-    const fetchProducts = async () => {
-        const response = await fetch('https://dummyjson.com/products');
+    const LIMIT = 30
+
+    const fetchProducts = async ({ pageParam = 0 }) => {
+        const response = await fetch(`https://dummyjson.com/products?limit=${LIMIT}&skip=${pageParam}`);
         return await response.json()
     }
 
-    const { data, error, isLoading } = useQuery<ProductResponse>({ queryKey: ['products'], queryFn: fetchProducts })
+    const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, status } = useInfiniteQuery(['products'], fetchProducts, {
+        getNextPageParam: (lastPage) => {
+            const nextSkip = lastPage.skip + LIMIT
+            return nextSkip > lastPage.total ? undefined : nextSkip
+        }
+    })
 
-    if (error) {
-        return (
-            <div className="mx-2 mt-4">
-                <p className="text-sm text-center">Something went wrong! Could not fetch products</p>
-            </div>
-        )
-    }
-
-    if (isLoading) {
-        <Loader />
-    }
-
-    return (
-        <div className="grid grid-cols-2 gap-4 mx-2 my-4 md:grid-cols-4 xl:grid-cols-6">
-            {
-                data && data.products.map((product: ProductType) => <ProductCard key={product.id} product={product} />)
-            }
+    return status === 'loading' ? (<Loader />) : status === 'error' ? (
+        <div className="mx-2 mt-4">
+            <p className="text-sm text-center">Something went wrong! Could not fetch products</p>
         </div>
+    ) : (
+        <>
+            <div className="grid grid-cols-2 gap-4 mx-2 my-4 md:grid-cols-4 xl:grid-cols-6">
+                {
+                    data && data.pages.map((group, index) => (
+                        <React.Fragment key={index}>
+                            {
+                                group.products.map((product: ProductType) => <ProductCard key={product.id} product={product} />)
+                            }
+                        </React.Fragment>
+                    ))
+                }
+            </div>
+            <div>
+                <button className="min-w-full py-4 my-4 text-center" onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
+                    {
+                        isFetchingNextPage
+                            ? 'Loading more...'
+                            : hasNextPage
+                                ? 'Load More'
+                                : 'Nothing more to load'
+                    }
+                </button>
+            </div>
+            <div className="min-w-full text-sm text-center">{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
+        </>
     )
 }
 
